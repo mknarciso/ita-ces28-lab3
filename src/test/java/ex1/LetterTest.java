074a91393bde6cf6259904e355c338728f0c624e
mknarciso <murilokn@gmail.com>
2016-10-15 18:28:38 +0000

    love letter

--- src/test/java/ex1/LetterTest.java
+++ src/test/java/ex1/LetterTest.java
@@ -11,15 +11,35 @@ public class LetterTest {
     Date date = new Date(15,10,2016);
     Address senderAddr = new Address("H9C 402");
     Address receiverAddr = new Address("H8A 112");
-    String result1 = "15/10/2016" + "\n\n" + "João" + "\n" +
-    "H9C 402" + "\n" + "Marcos" +
-    "H8A 112" + "\n" + "Dear " + "Marcos" + "\n" + "\nSincerely,\n" + "\n\n __________________\n" +
-    "João" + "\n " + "3333-1222" +
-    "\n email:" + "joao@gmail.com";
+    String content = "Here goes all the content of the letter. It doesn`t matter the language its written.";
+    String result1 = 
+    "15/10/2016\n\n" +
+    "From: \n  João\n  H9C 402\n" + 
+    "To: \n  Marcos\n  H8A 112\n\n" + 
+    "Dear Marcos,\n" + 
+    content + "\n" +
+    "\nSincerely,\n" +
+    "\n\n __________________\n" +
+    " João\n" + 
+    " 3333-1222\n"+
+    " email:" + "joao@gmail.com";
+    String city = "São José dos Campos";
+    String loveResult = "São José dos Campos, 15/10/2016\n\n" +
+    "Dear Marcos,\n" + content + "\n" + 
+    "\nWith love,\n" + 
+    "\n           João";
     @Test
     public void checkComercialLetter(){
         ComercialLetter comercial = new ComercialLetter(sender, receiver,
-                                                        senderAddr, receiverAddr, date);
+                                                        senderAddr, receiverAddr, content, date);
         assertEquals(result1,comercial.model());
+        System.out.println(comercial.model());
     }
+    @Test
+    public void checkLoveLetter(){
+        LoveLetter love = new LoveLetter(sender, receiver, city, content, date);
+        assertEquals(loveResult,love.model());
+        System.out.println(love.model());
+    }
+    
 }
